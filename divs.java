82a01101e9a426f9561ad7eceb53774c681759b3
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-06-21 10:53:10 +0100

    Add files via upload

---
+++ divs.java
@@ -0,0 +1,9 @@
+public class divs {
+    private static int divss(int a,int b){
+        return a/b;
+    }
+    public static void main(String[] args) {
+        int a=divss(1,1);
+        System.out.println(a);
+    }
+}
