c9d4ed58f96bc801e1b4f7525094fea1be9f39d1
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-06-21 10:55:16 +0100

    Add files via upload

--- divs.java
+++ divs.java
@@ -1,9 +1,5 @@
 public class divs {
-    private static int divss(int a,int b){
+    public  static int divss(int a,int b){
         return a/b;
-    }
-    public static void main(String[] args) {
-        int a=divss(1,1);
-        System.out.println(a);
     }
 }
--- divs.s
+++ divs.s
@@ -6,21 +6,10 @@ public class divs {
          1: invokespecial #1                  // Method java/lang/Object."<init>":()V
          4: return
 
-  private static int divss(int, int);
+  public static int divss(int, int);
     Code:
          0: iload_0
          1: iload_1
          2: idiv
          3: ireturn
-
-  public static void main(java.lang.String[]);
-    Code:
-         0: iconst_1
-         1: iconst_1
-         2: invokestatic  #7                  // Method divss:(II)I
-         5: istore_1
-         6: getstatic     #13                 // Field java/lang/System.out:Ljava/io/PrintStream;
-         9: iload_1
-        10: invokevirtual #19                 // Method java/io/PrintStream.println:(I)V
-        13: return
 }
